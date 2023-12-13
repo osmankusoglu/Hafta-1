fe44855d49f7fb1b4ae97de57440642d07ff8d7b
osmankusoglu <kusogluo@gmail.com>
2023-12-14 02:17:13 +0300

    Düzeltmeler

--- Aritmetik-Islemler-Ve-Islem-Onceligi/src/Main.java
+++ Aritmetik-Islemler-Ve-Islem-Onceligi/src/Main.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
 public class Main {
-    Scanner input = new Scanner(System.in);
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args){
-        int a= 10, b=2, c=3;
-        System.out.println(a+b*c-b);
+        // kullanıcı sayıları a= 10, b= 2, c= 3
+        int a = 10,b = 2 ,c = 3,sonuc = a+b*c-b;
+        //Kullanıcı verilen sayıları girdikçe değerler ve karşılıkları eşleşecek.
+        System.out.print("a sayısını giriniz : " );
+        a = input.nextInt();
+        System.out.print("b sayısını giriniz : " );
+        b = input.nextInt();
+        System.out.print("c sayısını giriniz : " );
+        c = input.nextInt();
+        //En son sayı girildiğinde istenilen sonucun çıktısı otomatik verilecek.
+        System.out.print("Sonuç = " + sonuc);
+        sonuc = input.nextInt();
 
     }
 }
