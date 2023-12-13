379f46839dea8eb3c365fb52e734d395c5b41ca2
osmankusoglu <kusogluo@gmail.com>
2023-12-14 01:46:06 +0300

    Aritmetik Işlemler ve Işlem Önceliği

---
+++ Aritmetik-Islemler-Ve-Islem-Onceligi/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    Scanner input = new Scanner(System.in);
+    public static void main(String[] args){
+        int a= 10, b=2, c=3;
+        System.out.println(a+b*c-b);
+
+    }
+}
