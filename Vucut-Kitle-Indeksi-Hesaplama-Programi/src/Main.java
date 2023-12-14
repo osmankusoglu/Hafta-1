0997e7c00b9fd49b45591205a7e47e504e2f9980
Osman Kuşoğlu <130009555+osmankusoglu@users.noreply.github.com>
2023-12-14 08:40:35 +0300

    Kullanıcı verilerin girişi için kod eklendi.

--- Vucut-Kitle-Indeksi-Hesaplama-Programi/src/Main.java
+++ Vucut-Kitle-Indeksi-Hesaplama-Programi/src/Main.java
@@ -7,8 +7,10 @@ public class Main {
 
         Scanner input= new Scanner(System.in);
 
-        System.out.println("Lütfen boyunuzu (metre cinsinde) giriniz : " + boy);
-        System.out.println("Lütfen kilonuzu giriniz : " + kilo );
+        System.out.println("Lütfen boyunuzu (metre cinsinde) giriniz : " );
+        boy = input.nextDouble();
+        System.out.println("Lütfen kilonuzu giriniz : " );
+        kilo = input.nextInt();
         System.out.println("Vücut Kitle İndeksiniz : " + endeks);
 
 
