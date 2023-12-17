cd9327e3e167ae12a1ba5925c1032a9da4ac7610
osmankusoglu <kusogluo@gmail.com>
2023-12-17 04:49:50 +0300

    Girilen Sayıya kadar hem 3'e hem de 4'e Bölünen Sayıların Ortalamasını Bulma

--- Manav-Kasa-Programi/src/Main.java
+++ Manav-Kasa-Programi/src/Main.java
@@ -1,27 +1,25 @@
 import java.util.Scanner;
 
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        int yil;
 
-        double armut = 2.14,elma = 3.67,domates = 1.11,muz = 0.95,patlıcan = 5.00;
-        double toplam = 0*armut+1*elma+1*domates+2*muz+3*patlıcan;
-
+        //Kullanıcıdan veri alma
         Scanner input = new Scanner(System.in);
+        System.out.print("Yıl giriniz :");
+        yil = input.nextInt();
 
-        System.out.print("Armut Kaç Kilo ? : ");
-        armut = input.nextDouble();
-        System.out.print("Elma Kaç Kilo ? : ");
-        elma = input.nextDouble();
-        System.out.print("Domates Kaç Kilo ? : ");
-        domates = input.nextDouble();
-        System.out.print("Muz Kaç Kilo ? : ");
-        muz = input.nextDouble();
-        System.out.print("Patlıcan Kaç Kilo ? : ");
-        patlıcan = input.nextDouble();
-        System.out.print("Toplam Tutar  : " + toplam + " TL");
-        toplam = input.nextDouble();
-
-
-
+        //Artık yıl bulma
+        if(yil % 4 == 0){
+            if( (yil % 100 == 0) && (yil % 400 != 0)){
+                System.out.println(yil + " bir artık yıl değildir ! !");
+            }else {
+                System.out.println(yil +" bir artık yıldır !");
+            }
+        }else {
+            System.out.println(yil +" bir artık yıl değildir !");
+        }
     }
 }
