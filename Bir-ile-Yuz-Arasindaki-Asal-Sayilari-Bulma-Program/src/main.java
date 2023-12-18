29afb96378c2fb4a2dda5b382627cdaa1a87dbf5
osmankusoglu <kusogluo@gmail.com>
2023-12-18 11:45:55 +0300

    Fibonacci Serisi

--- Bir-ile-Yuz-Arasindaki-Asal-Sayilari-Bulma-Program/src/main.java
+++ Bir-ile-Yuz-Arasindaki-Asal-Sayilari-Bulma-Program/src/main.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class main {
+public class main {
+    public static void main(String []args){
+        System.out.println();
+        for (int i= 2; i <=100 ;i++){
+            boolean sayi = true;
+            for (int k = 2; k<= Math.sqrt(i);k++){
+                if (i % k == 0) {
+                    sayi = false;
+                    break;
+                }
+            }
+            if (sayi){
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
