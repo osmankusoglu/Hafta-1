ae034aa0557ce8f367f1b8b8f4f1ede01ebc6bd7
osmankusoglu <kusogluo@gmail.com>
2023-12-18 23:01:37 +0300

    Asal Sayı Bulma

--- Bir-ile-Yuz-Arasindaki-Asal-Sayilari-Bulma-Program/src/main.java
+++ Bir-ile-Yuz-Arasindaki-Asal-Sayilari-Bulma-Program/src/main.java
@@ -10,7 +10,7 @@ public class main {
                 }
             }
             if (sayi){
-                System.out.print(i + " ");
+                System.out.print(i +  " ");
             }
         }
     }
